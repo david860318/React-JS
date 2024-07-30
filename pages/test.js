d4a4149e016e833b6cc02992dda475d8993bcb59
david860318 <david86031800@gmail.com>
2024-07-30 21:40:43 +0800

    練習

--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default function Test() {
+
+const foo = ()=>{
+  if(true){
+    console.log(123);
+  }
+}
+
   return (
     <>
-      <br></br>
-      <img src="" alt="" />
+      {foo()}
     </>
   );
 }
