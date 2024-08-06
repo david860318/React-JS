d7edbc5cb10d1fcfbf84bef9c5f9419d83f665b6
david860318 <david86031800@gmail.com>
2024-08-06 08:07:13 +0800

    0805

--- components/test/tickets.js
+++ components/test/tickets.js
@@ -11,6 +11,8 @@ import { IoIosCloseCircleOutline } from 'react-icons/io';
 import { FaCalendarCheck } from 'react-icons/fa';
 import { MdOutlineLowPriority } from 'react-icons/md';
 
+const statusOrder = { 待回覆: 1, 已處理: 2, 處理中: 3 };
+
 export default function Tickets() {
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = 5;
@@ -19,18 +21,24 @@ export default function Tickets() {
   const [showModal, setShowModal] = useState(false);
   const [showNestedModal, setShowNestedModal] = useState(false);
   const [showDetailModal, setShowDetailModal] = useState(false);
+  const [selectedTicket, setSelectedTicket] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // 處理表單提交
-    console.log('表單已提交');
-    setShowModal(false); // 提交後關閉彈出視窗
+    // console.log('客服單已提交');
+    setShowModal(false);
   };
 
   const handlePageClick = (page) => {
     if (page >= 1 && page <= totalPages) {
       setCurrentPage(page);
     }
+  };
+
+  const handleDetailClick = (ticket) => {
+    setSelectedTicket(ticket);
+    setShowDetailModal(true);
   };
 
   const handleNestedModalClick = () => {
@@ -40,6 +48,91 @@ export default function Tickets() {
   const handleNestedModalClose = () => {
     setShowNestedModal(false);
   };
+
+  // 排序狀態
+  const [sortField, setSortField] = useState('');
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  const handleSort = (field) => {
+    const newSortOrder =
+      sortField === field && sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortField(field);
+    setSortOrder(newSortOrder);
+  };
+
+  const data = [
+    {
+      id: '00001',
+      orderId: 'AA000123',
+      category: '營地相關',
+      description: '環境髒亂...',
+      createTime: '2024/07/12',
+      status: '已處理',
+    },
+    {
+      id: '00002',
+      orderId: 'AA000123',
+      category: '營地相關',
+      description: '環境髒亂...',
+      createTime: '2024/07/09',
+      status: '待回覆',
+    },
+    {
+      id: '00003',
+      orderId: 'BB000456',
+      category: '用品租借相關',
+      description: '用品破損...',
+      createTime: '2024/07/10',
+      status: '處理中',
+    },
+    {
+      id: '00004',
+      orderId: 'CC000789',
+      category: '費用相關',
+      description: '退款問題...',
+      createTime: '2024/07/11',
+      status: '已處理',
+    },
+    {
+      id: '00005',
+      orderId: 'BB000457',
+      category: '用品租借相關',
+      description: '用品丟失...',
+      createTime: '2024/07/12',
+      status: '處理中',
+    },
+  ];
+
+  const sortedData = [...data].sort((a, b) => {
+    if (sortField === 'id' || sortField === 'orderId') {
+      // 排序數字
+      const numA = parseInt(a[sortField].replace(/[^\d]/g, ''), 10);
+      const numB = parseInt(b[sortField].replace(/[^\d]/g, ''), 10);
+      if (numA < numB) return sortOrder === 'asc' ? -1 : 1;
+      if (numA > numB) return sortOrder === 'asc' ? 1 : -1;
+    } else if (sortField === 'createTime') {
+      // 排序日期
+      const dateA = new Date(a[sortField]);
+      const dateB = new Date(b[sortField]);
+      if (dateA < dateB) return sortOrder === 'asc' ? -1 : 1;
+      if (dateA > dateB) return sortOrder === 'asc' ? 1 : -1;
+    } else if (sortField === 'status') {
+      // 排序狀態
+      const statusA = statusOrder[a[sortField]];
+      const statusB = statusOrder[b[sortField]];
+      if (statusA < statusB) return sortOrder === 'asc' ? -1 : 1;
+      if (statusA > statusB) return sortOrder === 'asc' ? 1 : -1;
+      if (statusA === statusB) {
+        if (a.id < b.id) return sortOrder === 'asc' ? -1 : 1;
+        if (a.id > b.id) return sortOrder === 'asc' ? 1 : -1;
+      }
+    } else {
+      // 默認字母排序
+      if (a[sortField] < b[sortField]) return sortOrder === 'asc' ? -1 : 1;
+      if (a[sortField] > b[sortField]) return sortOrder === 'asc' ? 1 : -1;
+    }
+    return 0;
+  });
 
   return (
     <>
@@ -73,65 +166,49 @@ export default function Tickets() {
             <Table striped bordered hover className={styles.tableStyle}>
               <thead className={styles.theadStyle}>
                 <tr>
-                  <th>客服單編號</th>
-                  <th>訂單編號</th>
-                  <th>問題分類</th>
+                  <th onClick={() => handleSort('id')}>客服單編號</th>
+                  <th onClick={() => handleSort('orderId')}>訂單編號</th>
+                  <th onClick={() => handleSort('category')}>問題分類</th>
                   <th>問題說明</th>
-                  <th>建立時間</th>
-                  <th>檢視狀態</th>
+                  <th onClick={() => handleSort('createTime')}>建立時間</th>
+                  <th onClick={() => handleSort('status')}>檢視狀態</th>
                   <th>查看詳細</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className={styles.tdStyle}>
-                  <td>00001</td>
-                  <td>AA000123</td>
-                  <td>營地相關</td>
-                  <td className={styles.CsDescription}>環境髒亂...</td>
-                  <td>2024/07/09</td>
-                  <td className={styles.CsState}>
-                    <div className={styles.CsState1}>待回覆</div>
-                  </td>
-                  <td>
-                    <button
-                      href=""
-                      className={styles.checkBg}
-                      onClick={() => setShowDetailModal(true)}
-                    >
-                      <IoEyeOutline className={styles.checkIcon} />
-                    </button>
-                  </td>
-                </tr>
-                <tr className={styles.tdStyle}>
-                  <td>00001</td>
-                  <td>AA000123</td>
-                  <td>營地相關</td>
-                  <td className={styles.CsDescription}>環境髒亂...</td>
-                  <td>2024/07/09</td>
-                  <td className={styles.CsState}>
-                    <div className={styles.CsState2}>處理中</div>
-                  </td>
-                  <td>
-                    <button href="" className={styles.checkBg}>
-                      <IoEyeOutline className={styles.checkIcon} />
-                    </button>
-                  </td>
-                </tr>
-                <tr className={styles.tdStyle}>
-                  <td>00001</td>
-                  <td>AA000123</td>
-                  <td>營地相關</td>
-                  <td className={styles.CsDescription}>環境髒亂...</td>
-                  <td>2024/07/09</td>
-                  <td className={styles.CsState}>
-                    <div className={styles.CsState3}>已處理</div>
-                  </td>
-                  <td>
-                    <button href="" className={styles.checkBg}>
-                      <IoEyeOutline className={styles.checkIcon} />
-                    </button>
-                  </td>
-                </tr>
+                {sortedData.map((ticket) => (
+                  <tr key={ticket.id} className={styles.tdStyle}>
+                    <td>{ticket.id}</td>
+                    <td>{ticket.orderId}</td>
+                    <td>{ticket.category}</td>
+                    <td className={styles.CsDescription}>
+                      {ticket.description}
+                    </td>
+                    <td>{ticket.createTime}</td>
+                    <td className={styles.CsState}>
+                      <div
+                        className={
+                          ticket.status === '待回覆'
+                            ? styles.CsState1
+                            : ticket.status === '處理中'
+                            ? styles.CsState2
+                            : styles.CsState3
+                        }
+                      >
+                        {ticket.status}
+                      </div>
+                    </td>
+                    <td>
+                      <button
+                        href=""
+                        className={styles.checkBg}
+                        onClick={() => handleDetailClick(ticket)}
+                      >
+                        <IoEyeOutline className={styles.checkIcon} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </div>
@@ -341,8 +418,7 @@ export default function Tickets() {
         </div>
       )}
 
-      {/* 查看詳細頁 */}
-      {showDetailModal && (
+      {showDetailModal && selectedTicket && (
         <div className={styles.modalBackdrop}>
           <div className={styles.modalContent}>
             <div className={styles.modalClose}>
@@ -353,48 +429,65 @@ export default function Tickets() {
                 <IoIosCloseCircleOutline />
               </button>
             </div>
-            <form className={styles.form} onSubmit={handleSubmit}>
+            <form className={styles.form}>
               <div className={styles.formTitle}>客服單詳細資料</div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketEmail">客服單編號</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="ticketId">客服單編號</label>
+                  <div className={styles.DetailContent}>
+                    {selectedTicket.id}
+                  </div>
                 </div>
               </div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketPhone">訂單編號</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="orderId">訂單編號</label>
+                  <div className={styles.DetailContent}>
+                    {selectedTicket.orderId}
+                  </div>
                 </div>
               </div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketOrderNum">問題分類</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="category">問題分類</label>
+                  <div className={styles.DetailContent}>
+                    {selectedTicket.category}
+                  </div>
                 </div>
               </div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketCategory">建立時間</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="createTime">建立時間</label>
+                  <div className={styles.DetailContent}>
+                    {selectedTicket.createTime}
+                  </div>
                 </div>
               </div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketOrderNum">客服單狀態</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="status">客服單狀態</label>
+                  <div className={styles.DetailContent}>
+                    {selectedTicket.status}
+                  </div>
+                </div>
+              </div>
+              {/* 顯示客服回覆及回覆時間 */}
+              <div className={styles.formDetail}>
+                <div className={styles.formDetailContent}>
+                  <label htmlFor="reply">客服回覆</label>
+                  <div className={styles.DetailContent}>
+                    {/* 可以顯示或是獲取客服回覆內容 */}
+                    {selectedTicket.reply || '尚無回覆'}
+                  </div>
                 </div>
               </div>
               <div className={styles.formDetail}>
                 <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketOrderNum">客服回覆</label>
-                  <div className={styles.DetailContent}>00001</div>
-                </div>
-              </div>
-              <div className={styles.formDetail}>
-                <div className={styles.formDetailContent}>
-                  <label htmlFor="ticketOrderNum">回覆時間</label>
-                  <div className={styles.DetailContent}>00001</div>
+                  <label htmlFor="replyTime">回覆時間</label>
+                  <div className={styles.DetailContent}>
+                    {/* 可以顯示回覆時間 */}
+                    {selectedTicket.replyTime || '尚無回覆時間'}
+                  </div>
                 </div>
               </div>
               <div className={styles.submitGroup}>
